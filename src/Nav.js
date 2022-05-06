2b537a0cc79725193517908e3ebcf0bff1a7af2b
Kiyoko <kiyoko113@yahoo.co.jp>
2022-05-05 19:06:03 -0700

    first commit

---
+++ src/Nav.js
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from 'react';
+import './Nav.css';
+
+function Nav() {
+  const [show, handleShow] = useState(false);
+  useEffect(() => {
+    window.addEventListener('scroll', () => {
+      if (window.scrollY > 100) {
+        handleShow(true);
+      } else handleShow(false);
+    });
+    return () => {
+      window.removeEventListener('scroll');
+    };
+  }, []);
+  return (
+    <div className={`nav ${show && 'nav__black'}`}>
+      {/* <div className="nav"> */}
+      <img
+        className="nav__logo"
+        // src="https://upload.wikimedia.org/wikipedia/commons/6/67/NewNetflixLogo.png"
+        // src="https://i.ibb.co/C0V37Jn/39fb1995180d6b51f1cdc816e8b0ada3.png"
+        src="https://fontmeme.com/permalink/220505/b1a3ae862796559d435be18c00c4b007.png"
+        alt="Netflix Logo"
+      />
+
+      <img
+        className="nav__avatar"
+        src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
+        alt="Netflix Logo"
+      />
+    </div>
+  );
+}
+
+export default Nav;
